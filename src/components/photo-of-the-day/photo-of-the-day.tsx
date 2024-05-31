e033b705f79c7f1a30a672a3f9292742c2e459b1
mjwideburg <michael.wideburg@efwnow.com>
2024-05-31 08:33:44 -0400

    fix(style): update photo style and media type

--- src/components/photo-of-the-day/photo-of-the-day.tsx
+++ src/components/photo-of-the-day/photo-of-the-day.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardHeader, CardMedia, Container, Typography } from '@mui/material';
+import { getPhotoOfTheDay } from '../../api/nasa';
 
 const PhotoOfTheDay: React.FC = ({}) => {
   const [photoData, setPhoto] = useState<any>()
@@ -8,23 +9,32 @@ const PhotoOfTheDay: React.FC = ({}) => {
 
   return (
     <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-      <Typography variant="h2" component="h2">
+      <Typography variant="h4" component="h2">
         Photo Of The Day
       </Typography>
       {photoData && (
-        <Card variant="outlined" sx={{ maxWidth: 600, width: '100%', mt: 4 }}>
-          <CardHeader title={photoData.title} />
+        <Card variant="outlined" sx={{ maxWidth: 800, width: '100%', mt: 4, mb:5 }}>
+        <CardHeader title={photoData.title} />
+        {photoData.media_type === "video" ? (
+          <iframe
+            src={photoData.url}
+            title={photoData.title}
+            style={{ width: '100%', height: 300, border: 'none' }}
+            allowFullScreen
+          />
+        ) : (
           <CardMedia
             component="img"
             image={photoData.url}
             alt={photoData.title}
           />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {photoData.explanation}
-            </Typography>
-          </CardContent>
-        </Card>
+        )}
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {photoData.explanation}
+          </Typography>
+        </CardContent>
+      </Card>
       )}
     </Container>
   );
